f69906eb9ccda3d15cd43e2ccc4bcd26e234fce6
leidyegg <lgladino@gmail.com>
2023-10-17 20:00:28 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//alert("Hola este es mi Javascript");
+let nombre = "Leidy";
+console.log(nombre)
+
+// SELECCIONAR ELEMENTOS
+
+let titulo = document.querySelector(".logo.fuente")
+let contenidotitulo = "acerca de mi";
+titulo.innerHTML = contenidotitulo;
+
+//CONDICIONALES
+let textotitulo = titulo.innertext;
+console.log (acerca-de-mi);
+
+if(textotitulo == "acerca de mi"){
+    titulo.innerHTML = "otro";
+} else {
+    console.log("no se cumple")
+}
